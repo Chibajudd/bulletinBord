d04f9823e7f7d218b1f030ffc343455978bd7975
FukeTakumi <t6cd24ac27b@gmail.com>
2020-04-20 19:53:53 +0900

    add environment variables

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-{
+require('dotenv').config();
+module.exports = {
   "development": {
     "username": "root",
     "password": null,
@@ -16,10 +17,10 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "bulletinBord_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "operatorsAliases": false
   }
